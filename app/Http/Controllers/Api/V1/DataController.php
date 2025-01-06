82f6e361820aac686bc2253059b2ba987395ea2e
Bakoulis George <g.bakoulis@outlook.com>
2025-01-06 10:28:12 +0200

    API query filter

---
+++ app/Http/Controllers/Api/V1/DataController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\Data;
+use Illuminate\Http\Request;
+
+class DataController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Data $data)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Data $data)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Data $data)
+    {
+        //
+    }
+}
---
+++ app/Http/Filters/V1/DataFilter.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Http\Filters\V1;
+
+class DataFilter extends QueryFilter
+{
+
+}
