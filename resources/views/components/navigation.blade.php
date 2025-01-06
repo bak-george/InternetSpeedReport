30dd1324da7695ec5adab66f0a882a0667e37446
Bakoulis George <g.bakoulis@outlook.com>
2025-01-06 10:07:53 +0200

    removed about component

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,6 +1,5 @@
 <nav class="w-full flex justify-center p-2 lg:px-8" aria-label="Global">
   <div class="hidden lg:flex lg:justify-center lg:gap-x-6">
-    <a href="#" class="text-lg ubuntu-medium  drop-shadow-2xl">About</a>
     <a href="{{ url('api') }}" class="text-lg ubuntu-medium  drop-shadow-2xl">API</a>
   </div>
 </nav>
